bfb204f181ddebfb36122295f77657b7dd49c45c
Kevin Silas <Kwsilas@gmail.com>
2015-08-24 17:49:57 -0400

    add html css grid for TTT

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,18 @@
 // jshint devel:true
 console.log('Here we go!');
+//Define players, gameboard, and other variables.
+var player1 = 'x';
+var player2 = 'o';
+
+var gameboard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+
+//player chooses a space and undates html.
+
+//Determine when either player1 or player2 wins.
+
+
+//Switch between player1 and player2
+
+//Keeping score
+
+//reseting the board.
