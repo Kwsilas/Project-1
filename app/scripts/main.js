c576f7e89b79ecd78971ceaabe6c52114a051c61
Kevin Silas <Kwsilas@gmail.com>
2015-08-25 16:03:21 -0400

    work on winner logic and tracking player moves

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,62 +1,100 @@
 // jshint devel:true
 
 console.log('Here we go!');
-'use strict'
 
-//Define players, gameboard, and other variables.
-//var player1 = 'x';
-//var player2 = 'o';
-
-var gameboard = [
-  ['b1', 'b2', 'b3'],
-  ['b4', 'b5', 'b6'],
-  ['b7', 'b8', 'b9']
-];
-
-var winCombo = [
-  ['b1', 'b2', 'b3'],
-  ['b4', 'b5', 'b6'],
-  ['b7', 'b8', 'b9'],
-  ['b1', 'b4', 'b7'],
-  ['b2', 'b5', 'b8'],
-  ['b3', 'b6', 'b9'],
-  ['b1', 'b5', 'b9'],
-  ['b3', 'b5', 'b7']
-];
+'use strict';
 
 $(document).ready(function() {
-  //player chooses a space and undates html.
+
+  //Define players, gameboard, and other variables.
+  var player1 = 'X';
+  var player2 = 'O';
   var turn = 0;
-  $('#b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9')
-    .click(function() {
-      var box = $(this);
-      if (turn == 0) {
-        turn = 1;
-        box.text('X');
-      }
-      else {
-        turn = 0;
-        box.text('O');
-      }
-    });
+  var player;
+  var p1Moves = [];
+  var p2Moves = [];
+
+  var winCombo = [
+    ['b1', 'b2', 'b3'],
+    ['b4', 'b5', 'b6'],
+    ['b7', 'b8', 'b9'],
+    ['b1', 'b4', 'b7'],
+    ['b2', 'b5', 'b8'],
+    ['b3', 'b6', 'b9'],
+    ['b1', 'b5', 'b9'],
+    ['b3', 'b5', 'b7']
+  ];
+
+  //Switch between player1 and player2
+  var picTurn = function() {
+    if (turn % 2 == 0) {
+      player = 'X';
+    }
+    else {
+      player = 'O';
+    }
+    return player;
+
+  };
+
+  //Add moves for player1 or player2 arrays.
+  var trackMoves = function(loc) {
+    if (picTurn() == 'X') {
+      p1Moves.push(loc);
+    }
+    else if (picTurn() == 'O') {
+      p2Moves.push(loc);
+    }
+  };
+
+  //Determine winner.
+
+  //function playerWins() {
+  //if (p1Moves == winCombo[]);
+  //alert('X wins!');
+  // }
+  //else if {(p2Moves == winCombo[]);
+  //alert('O wins!')
+  //}
+  //else alert('Tie.');
+  //}
+
+
+  //get winner function here
+
+  winCombo.forEach(function(arr){
+    if(_.isEqual(arr, p1Moves)) {
+     // alert ('X is winner!');
+    } else if (_.isEqual(arr, p2Moves)) {
+    //  alert ('O is winner!');
+    } else{
+     // alert ('It is a tie!');
+    }//same thing for p2
+  });
+
+  //player chooses a space and undates game and html.
+
+  $('.col-md-3').on('click', function(event) {
+    trackMoves(event.target.id);
+    picTurn;
+    $(this).text(picTurn);
+    turn++;
+
+    console.log(turn, p1Moves, p2Moves);
+    //get winner call here
+  });
+
 });
 
 
-//Determine when either player1 or player2 wins.
-
-function playerWins() {
-  if ('X' == winCombo[]);
-  alert('X wins!')
- }
-  {else if ('O' == winCombo[]);
-  alert('O wins!')
- }
-  else alert('Tie.');
-  }
-};
 
 
-//Switch between player1 and player2
+
+
+
+
+
+
 
 //Keeping score
 
