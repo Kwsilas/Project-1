2893731eade3e0f5251dae86d2ba2ca575371bce
Kevin Silas <Kwsilas@gmail.com>
2015-08-26 11:02:26 -0400

    Final fix on win and tie logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,8 +7,7 @@ console.log('Here we go!');
 $(document).ready(function() {
 
   //Define players, gameboard, and other variables.
-  var player1 = 'X';
-  var player2 = 'O';
+  var winner = undefined;
   var turn = 0;
   var player;
   var p1Moves = [];
@@ -27,7 +26,7 @@ $(document).ready(function() {
 
   //Switch between player1 and player2
   var picTurn = function() {
-    if (turn % 2 == 0) {
+    if (turn % 2 === 0) {
       player = 'X';
     }
     else {
@@ -39,33 +38,44 @@ $(document).ready(function() {
 
   //Add moves for player1 or player2 arrays.
   var trackMoves = function(loc) {
-    if (picTurn() == 'X') {
+    if (picTurn() === 'X') {
       p1Moves.push(loc);
       p1Moves.sort();
     }
-    else if (picTurn() == 'O') {
+    else if (picTurn() === 'O') {
       p2Moves.push(loc);
       p2Moves.sort();
     }
   };
 
+  function checkForWinner(winningCombo, moves) {
+    var result = _.includes(moves, winningCombo[0]) && _.includes(moves, winningCombo[1]) && _.includes(moves, winningCombo[2])
+    console.log('checkForWinner returning', result, 'for moves', JSON.stringify(moves));
+    return result;
+  }
+
   //get winner function here
   var getWinner = function() {
-    winCombo.forEach(function(arr) {
-      if (_.isEqual(arr, p1Moves)) {
-        player1 = 'Winner';
+    for (var i = 0; i < winCombo.length; i++) {
+      var arr = winCombo[i];
+      if (checkForWinner(arr, p1Moves)) {
+        winner = 'X';
         alert('X wins!');
+        console.log('=== X WINS ===');
+        break;
       }
-      else if (_.isEqual(arr, p2Moves)) {
-        player2 = 'Winner';
+      else if (checkForWinner(arr, p2Moves)) {
+        winner = 'O';
         alert('O wins!');
-      } else if (turn === 9) {
-        alert ('Tie');
-        return;
+        console.log('=== O WINS ===');
+        break;
       }
-    })
+    }
+    if (!winner && turn === 9) {
+      alert('Tie');
+      console.log('=== TIE ===');
+    }
   };
-
 
   //player chooses a space and undates game and html.
 
